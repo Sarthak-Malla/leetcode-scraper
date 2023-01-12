4d929a55fdd66c069fc8a5a1dd11bae02417b35f
Sarthak-Malla <spm9513@nyu.edu>
2023-01-12 17:48:22 +0400

    Folder creation and modularized code
    Made the code more readable and modularized it. The code now creates a
    folder for each problem, adds a main.cpp file with the include boilerplate
    and a main function. The code also creates a .gitignore file with *.out

--- main.py
+++ main.py
@@ -1,39 +1,81 @@
 from selenium import webdriver
 import bs4
 import os
+import sys
 
-driver = webdriver.Chrome()
-driver.get("https://leetcode.com/problems/two-sum/")
+# path to my LeetCode git repo
+PATH = "../../LeetCode/"
 
-driver.implicitly_wait(3)
+# create a folder with the name of the problem if not exists
+def create_folder(folder_name):
+    path_ = f"{PATH}{folder_name}"
+    if not os.path.exists(path_):
+        os.mkdir(path_)
+    else:
+        print(f"Folder {folder_name} already exists")
+        exit()
 
-soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+    print("------------------------------")
+    print(f"Folder {folder_name} created!")
+    print("------------------------------")
 
-# title
-title = soup.find('div', class_='h-full').find('span', class_='mr-2').text
-title_l = title.split()[1:] # remove the number
-title = ' '.join(title_l) # join the list
-folder_name = "".join(title_l)
+# write README.md file
+def write_readme(title, difficulty, description_tag, folder_name):
+    with open(f'{PATH}{folder_name}/README.md', 'w') as file:
+        file.write(f"# {title}\n\n")
+        file.write(f"Difficulty: {difficulty}\n")
+        file.write(f"## Description\n")
 
-difficulty = soup.find('div', class_='mt-3').div.div.text
+        for line in description_tag:
+            if line != '\n':
+                file.write(f"{line}\n")
+    print("------------------------------")
+    print(f"README.md file created!")
+    print("------------------------------")
 
-description_tag = soup.find('div', class_='_1l1MA').contents
+    # create .gitignore file
+    with open(f'{PATH}{folder_name}/.gitignore', 'w') as file:
+        file.write(f"*.out\n")
 
-print(description_tag)
+    # create main.cpp file
+    with open(f"{PATH}{folder_name}/main.cpp", 'w') as file:
+        file.write(f"#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main(){{\n\n\treturn 0;\n}}")
 
-# create a folder with the name of the problem
-path = f"./{folder_name}"
-if not os.path.exists(path):
-    os.mkdir(path)
+# runs selenium to extract the problem description
+def extract_problem(url):    
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-# storing the problem description in a README.md file
-with open(f'./{folder_name}/README.md', 'w') as file:
-    file.write(f"# {title}\n\n")
-    file.write(f"Difficulty: {difficulty}\n")
-    file.write(f"## Description\n")
+    driver.implicitly_wait(3)
 
-    for line in description_tag:
-        if line != '\n':
-            file.write(f"{line}\n")
+    soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
 
-driver.quit()
+    # title
+    title = soup.find('div', class_='h-full').find('span', class_='mr-2').text
+    folder_name = title.strip(); # folder name
+    title_l = title.split()[1:] # remove the number
+    title = ' '.join(title_l) # join the list
+
+    difficulty = soup.find('div', class_='mt-3').div.div.text
+
+    description_tag = soup.find('div', class_='_1l1MA').contents
+
+    create_folder(folder_name)
+
+    # storing the problem description in a README.md file
+    write_readme(title, difficulty, description_tag, folder_name)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <url>")
+        exit()
+
+    url = sys.argv[1]
+
+    if not url.startswith("https://leetcode.com/problems/"):
+        print("Invalid URL")
+        exit()
+
+    extract_problem(url)
